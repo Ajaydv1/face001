4eed040a8a3ed2560d2dba2223c42a987c508e0e
Ajay <ajaydv1999@gmail.com>
2023-11-24 11:10:23 +0530

    facebook git work

---
+++ facebook github/src/facebook/Facebook.java
@@ -0,0 +1,24 @@
+package facebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Facebook {
+
+	@Test
+	
+public void login()
+{
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\ajayd\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("https://WWW.facebook.com");
+		driver.findElement(By.name("email")).sendKeys("ajaydv@gmail.com");
+		driver.findElement(By.name("pass")).sendKeys("ajay12345");
+		driver.findElement(By.name("login")).click();
+		
+}
+}
